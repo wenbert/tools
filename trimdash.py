b480a7586d00c72343a7beaeeee9f6c92db707c8
wenbert <wenbert@ubuntu-wenbert.(none)>
2010-12-05 11:51:37 +0100

    some cleanup.

--- trimdash.py
+++ trimdash.py
@@ -1,6 +1,10 @@
 """
 Usage:
-$ python trimdash.py sourcedirectory targetdirectory
+$ python trimdash.py sourcedirectory
+
+Creates a "nodash_XXX" directory containing the XYZ with no dashes.
+XXX is a timestamp formated: YYYYMMDDHHMMSS 
+Example: nodash_20101205141004
 """
 import os, sys, datetime, calendar
 from sys import argv
@@ -10,30 +14,24 @@ script, input_dir = argv
 
 try:
     input_dir = os.path.abspath(input_dir)
-    output_dir = "converted_%s" % strftime("%Y%m%d_%H_%M_%S", gmtime())
-    print input_dir
-    
+    output_dir = "nodash_%s" % strftime("%Y%m%d_%H_%M_%S", gmtime())
+    print "Opening directory: %s" % input_dir
     
     contents = os.listdir(str(input_dir)) 
+    total_files = len(contents)
     
-    #os.mkdir("%s/%s", os.path.join(input_dir, "converted"))
     os.mkdir(output_dir)
-    print os.path.abspath(output_dir)
+    print "Output directory: %s" % os.path.abspath(output_dir)
     
     count = 1
+    
     for i in contents:
-        #filename = i
-        #i = os.path.join(input_dir,i)
-        
-        
-        #MAKE SURE TO CHECK THAT THE FILE IS XYZ!
-        
         output_dir = os.path.join(os.path.abspath(output_dir))
         
         new_file = open(os.path.join(output_dir,i), 'w')
         new_file.truncate()
         
-        print "opening file: ", i,
+        print "Processing file (%s of %s): %s"% (count, total_files, i),
         
         file_extension = i.split(".")[-1]
         
@@ -42,14 +40,14 @@ try:
                 line = line.replace("-", "")
                 new_file.write(line)
         else:
-            print "%s is not an XYZ file! skipping... " % i
-        print " closing..."
+            print "[not an XYZ file! skipping]",
+        print " closing."
         
         new_file.close()
         
         count += 1
-    print "processed. converted contents of: %s to %s" \
-          % (input_dir, os.path.abspath(output_dir))
+    print "Done. Output files created in %s" \
+          % (os.path.abspath(output_dir))
           
 except:
     print "Unexpected error",  sys.exc_info()
